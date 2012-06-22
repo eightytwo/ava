49d75c0072e8a0f0507bf45d9d331be33f30731f
eightytwo <eightytwo@users.noreply.github.com>
2012-06-22 23:38:04 +1000

    Added basic unit tests for Organisation.

--- test/unit/organisation_test.rb
+++ test/unit/organisation_test.rb
@@ -1,7 +1,20 @@
 require 'test_helper'
 
 class OrganisationTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test "create with missing details" do
+    organisation = Organisation.new
+    assert !organisation.save, "Saved the organisation without any details."
+  end
+
+  test "update with nil name" do
+    organisation = Organisation.find(organisations(:marlinspike).id)
+    organisation.name = nil
+    assert !organisation.save, "Updated an organisation with a nil name."
+  end
+
+  test "update with blank name" do
+    organisation = Organisation.find(organisations(:marlinspike).id)
+    organisation.name = ""
+    assert !organisation.save, "Updated an organisation with a blank name."
+  end
 end
