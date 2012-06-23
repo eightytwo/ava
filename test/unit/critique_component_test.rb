c062652c88c48c8ca5251da4c63671dd3d53bff7
eightytwo <eightytwo@users.noreply.github.com>
2012-06-24 03:12:39 +1000

    Added unit tests for CritiqueComponent.

--- test/unit/critique_component_test.rb
+++ test/unit/critique_component_test.rb
@@ -1,7 +1,63 @@
 require 'test_helper'
 
 class CritiqueComponentTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test "create with missing details" do
+    component = CritiqueComponent.new
+    
+    assert !component.save, "Saved a critique component without any details."
+  end
+
+  test "update with nil critique" do
+    component = CritiqueComponent.find(
+      critique_components(:haddock_critique_one_component_one).id)
+    component.critique_id = nil
+    
+    assert(
+      !component.save,
+      "Saved a component with a nil critique reference.")
+  end
+
+  test "update with a nonexistent critique" do
+    component = CritiqueComponent.find(
+      critique_components(:haddock_critique_one_component_one).id)
+    component.critique_id = 0
+    
+    begin
+      component.save
+      assert(
+        false,
+        "Saved a component with a reference to a nonexistent critique.")
+    rescue ActiveRecord::InvalidForeignKey
+      assert true
+    rescue Exception => e
+      assert false, e.message
+    end
+  end
+
+  test "update with nil critique category" do
+    component = CritiqueComponent.find(
+      critique_components(:haddock_critique_one_component_one).id)
+    component.critique_category_id = nil
+    
+    assert(
+      !component.save,
+      "Saved a component with a nil critique category reference.")
+  end
+
+  test "update with a nonexistent user" do
+    component = CritiqueComponent.find(
+      critique_components(:haddock_critique_one_component_one).id)
+    component.critique_category_id = 0
+    
+    begin
+      component.save
+      assert(
+        false,
+        "Saved a component with a reference to a nonexistent critique category.")
+    rescue ActiveRecord::InvalidForeignKey
+      assert true
+    rescue Exception => e
+      assert false, e.message
+    end
+  end
 end
