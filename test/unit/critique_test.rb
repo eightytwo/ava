9d03cb91196c053d833094f65e8d7966def6fccf
eightytwo <eightytwo@users.noreply.github.com>
2012-06-24 02:52:18 +1000

    Added unit tests to Critique.

--- test/unit/critique_test.rb
+++ test/unit/critique_test.rb
@@ -1,7 +1,57 @@
 require 'test_helper'
 
 class CritiqueTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test "create with missing details" do
+    critique = Critique.new
+    
+    assert !critique.save, "Saved a critique without any details."
+  end
+
+  test "update with nil round audio visual" do
+    critique = Critique.find(critiques(:haddock_critique_on_tintin_av).id)
+    critique.round_audio_visual_id = nil
+    
+    assert(
+      !critique.save,
+      "Saved a critique with a nil round audio visual reference.")
+  end
+
+  test "update with a nonexistent round audio visual" do
+    critique = Critique.find(critiques(:haddock_critique_on_tintin_av).id)
+    critique.round_audio_visual_id = 0
+    
+    begin
+      critique.save
+      assert(
+        false,
+        "Saved a critique with a reference to a nonexistent round audio visual.")
+    rescue ActiveRecord::InvalidForeignKey
+      assert true
+    rescue Exception => e
+      assert false, e.message
+    end
+  end
+
+  test "update with nil user" do
+    critique = Critique.find(critiques(:haddock_critique_on_tintin_av).id)
+    critique.user_id = nil
+    
+    assert !critique.save, "Saved a critique with a nil user reference."
+  end
+
+  test "update with a nonexistent user" do
+    critique = Critique.find(critiques(:haddock_critique_on_tintin_av).id)
+    critique.user_id = 0
+    
+    begin
+      critique.save
+      assert(
+        false,
+        "Saved a critique with a reference to a nonexistent user.")
+    rescue ActiveRecord::InvalidForeignKey
+      assert true
+    rescue Exception => e
+      assert false, e.message
+    end
+  end
 end
