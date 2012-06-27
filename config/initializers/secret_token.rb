250d994da120f39d9b0b833a2388f2e67b015223
eightytwo <eightytwo@users.noreply.github.com>
2012-06-28 01:59:13 +1000

    Renamed the secret token environment variable.

--- config/initializers/secret_token.rb
+++ config/initializers/secret_token.rb
@@ -5,7 +5,7 @@
 # Make sure the secret is at least 30 characters and all random,
 # no regular words or you'll be exposed to dictionary attacks.
 unless Rails.env.production?
-  ENV['secret_token'] = '2002733de8ffdbfb57cabf40a9defb3ad1ed3bcdb32d92a4200777757f5a97ab704f66c15b3916293580a9d44336ed47395b40ca2736ceeb35a63a9d993edacd'
+  ENV['AVA_SECRET_TOKEN'] = '2002733de8ffdbfb57cabf40a9defb3ad1ed3bcdb32d92a4200777757f5a97ab704f66c15b3916293580a9d44336ed47395b40ca2736ceeb35a63a9d993edacd'
 end
 
-Ava::Application.config.secret_token = ENV['secret_token']
+Ava::Application.config.secret_token = ENV['AVA_SECRET_TOKEN']
