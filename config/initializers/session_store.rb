17e0f33a52a5d63c56deec991bb4b98d0c812d7a
eightytwo <eightytwo@users.noreply.github.com>
2012-06-27 19:05:17 +1000

    Session secret is now derived from environment variable. Cookie marked as httponly and ssl.

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,6 +1,10 @@
 # Be sure to restart your server when you modify this file.
 
-Ava::Application.config.session_store :cookie_store, key: '_ava_session'
+Ava::Application.config.session_store :cookie_store, {
+  key: '_ava_session',
+  secure: Rails.env.production?,
+  httponly: true
+}
 
 # Use the database for sessions instead of the cookie-based default,
 # which shouldn't be used to store highly confidential information
