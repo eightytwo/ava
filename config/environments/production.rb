f37b21ae5630b681e3a9e2ca09b1283445885612
eightytwo <eightytwo@users.noreply.github.com>
2012-07-02 16:13:31 +1000

    Updated the domain for emails.

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -50,7 +50,7 @@ Ava::Application.configure do
 
   # Disable delivery errors, bad email addresses will be ignored
   # config.action_mailer.raise_delivery_errors = false
-  config.action_mailer.default_url_options = { :host => 'localhost:3002' }
+  config.action_mailer.default_url_options = { :host => 'avarcade.herokuapp.com' }
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.smtp_settings = {
     :address        => 'smtp.sendgrid.net',
