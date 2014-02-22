e95900cadb2fd342be39f6daed1c8ea0c1f0eff5
eightytwo <eightytwo@users.noreply.github.com>
2014-02-22 10:53:24 +0000

    Add queries for setting up an organisation

--- db/queries.sql
+++ db/queries.sql
@@ -6,3 +6,17 @@ FROM    users
         INNER JOIN organisation_users on users.id = organisation_users.user_id
 WHERE   organisation_users.organisation_id = 1
 ORDER BY sign_in_count DESC;
+
+
+--
+-- Create organisation
+--
+INSERT INTO organisations (name, description, website, created_at, updated_at)
+VALUES      ('', '', '', now(), now());
+
+
+--
+-- Add admin to the organisation
+--
+INSERT INTO organisation_users (organisation_id, user_id, admin, created_at, updated_at)
+VALUES      (x, 1, 1, now(), now())
